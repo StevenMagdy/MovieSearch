e7b4bb5a6e245acc513e31f2ca620f1d04bc745e
Steven <steven@Stevens-MacBook-Pro.local>
2017-05-10 22:50:09 +0200

    Design details activity

--- app/src/main/java/com/steven/moviesearch/DetailsActivity.java
+++ app/src/main/java/com/steven/moviesearch/DetailsActivity.java
@@ -5,7 +5,10 @@ import android.content.Loader;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -15,8 +18,10 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager
 
 	private String url = "http://www.omdbapi.com/";
 	private String imdbID;
-	TextView titleTextView, yearTextView, ratedTextView, ratingTextView, genreTextView,
-			runtimeTextView, plotTextView;
+	TextView titleYearTextView, ratedTextView, imdbRatingTextView, genreTextView,
+			runtimeTextView, plotTextView, writerTextView, actorsTextView, directorTextView,
+			languageTextView;
+	ImageView posterImageView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +29,17 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager
 		setContentView(R.layout.activity_details);
 		imdbID = getIntent().getStringExtra("imdbID");
 		getLoaderManager().initLoader(2, null, this);
-		titleTextView = (TextView) findViewById(R.id.titleTextView);
-		yearTextView = (TextView) findViewById(R.id.yearTextView);
-		ratedTextView = (TextView) findViewById(R.id.ratedTextView);
-		ratingTextView = (TextView) findViewById(R.id.ratingTextView);
-		genreTextView = (TextView) findViewById(R.id.genreTextView);
-		runtimeTextView = (TextView) findViewById(R.id.runtimeTextView);
-		plotTextView = (TextView) findViewById(R.id.plotTextView);
-
+		titleYearTextView = (TextView) findViewById(R.id.textView_title_year);
+		ratedTextView = (TextView) findViewById(R.id.textView_rated);
+		imdbRatingTextView = (TextView) findViewById(R.id.textView_imdbRating);
+		genreTextView = (TextView) findViewById(R.id.textView_genre);
+		runtimeTextView = (TextView) findViewById(R.id.textView_runtime);
+		plotTextView = (TextView) findViewById(R.id.textView_plot);
+		writerTextView = (TextView) findViewById(R.id.textView_writer);
+		actorsTextView = (TextView) findViewById(R.id.textView_actors);
+		directorTextView = (TextView) findViewById(R.id.textView_director);
+		languageTextView = (TextView) findViewById(R.id.textView_language);
+		posterImageView = (ImageView) findViewById(R.id.imageView_poster);
 
 	}
 
@@ -46,13 +54,22 @@ public class DetailsActivity extends AppCompatActivity implements LoaderManager
 	@Override
 	public void onLoadFinished(Loader<JSONObject> loader, JSONObject data) {
 		try {
-			titleTextView.setText(data.getString("Title"));
-			yearTextView.setText("(" + data.getString("Year") + ")");
+			String titleYear = data.getString("Title") + " (" + data.getString("Year") + ")";
+			titleYearTextView.setText(titleYear);
 			ratedTextView.setText(data.getString("Rated"));
-			ratingTextView.setText(data.getString("imdbRating"));
+			imdbRatingTextView.setText(data.getString("imdbRating"));
 			genreTextView.setText(data.getString("Genre"));
 			runtimeTextView.setText(data.getString("Runtime"));
 			plotTextView.setText(data.getString("Plot"));
+			writerTextView.setText(data.getString("Writer"));
+			actorsTextView.setText(data.getString("Actors"));
+			directorTextView.setText(data.getString("Director"));
+			languageTextView.setText(data.getString("Language"));
+			Glide.with(this)
+					.load(data.getString("Poster"))
+					.centerCrop()
+					.into(posterImageView);
+
 		} catch (JSONException e) {
 
 		}
